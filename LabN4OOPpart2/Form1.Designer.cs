84ab66c4d4421ce902e9086ae425d924f66f3bf6
MeRkyLeZ <robert.avhadiev.01@mail.ru>
2020-11-27 20:43:57 +0500

    Добавлена реализация без MVC

--- LabN4OOPpart2/Form1.Designer.cs
+++ LabN4OOPpart2/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- LabN4OOPpart2/Form1.cs
+++ LabN4OOPpart2/Form1.cs
@@ -17,5 +17,61 @@ namespace LabN4OOPpart2
             InitializeComponent();
         }
 
+        private void CheckNull()    // Проверка пустоты
+        {
+            if (textBox1.Text == "") textBox1.Text = (0).ToString();
+            if (textBox2.Text == "") textBox2.Text = (0).ToString();
+            if (textBox3.Text == "") textBox3.Text = (0).ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CheckNull();
+            if (Int32.Parse(textBox1.Text) <= Int32.Parse(textBox2.Text))
+            {
+                textBox2.Text = (Int32.Parse(textBox1.Text) - 1).ToString();
+            }
+            textBox3.Text = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            CheckNull();
+            if (Int32.Parse(textBox1.Text) <= Int32.Parse(textBox2.Text))
+            {
+                textBox1.Text = (Int32.Parse(textBox2.Text) + 1).ToString();
+            }
+            textBox3.Text = textBox2.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            CheckNull();
+            if (Int32.Parse(textBox1.Text) == Int32.Parse(textBox3.Text))
+            {
+                textBox1.Text = textBox3.Text;
+                if (Int32.Parse(textBox1.Text) <= Int32.Parse(textBox2.Text))
+                {
+                    textBox2.Text = (Int32.Parse(textBox1.Text) - 1).ToString();
+                }
+            }
+            else
+            {
+                textBox2.Text = textBox3.Text;
+                if (Int32.Parse(textBox1.Text) <= Int32.Parse(textBox2.Text))
+                {
+                    textBox1.Text = (Int32.Parse(textBox2.Text) + 1).ToString();
+                }
+            }
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)   // Ввод только чисел
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
